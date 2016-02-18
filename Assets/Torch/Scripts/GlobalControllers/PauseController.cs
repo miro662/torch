86d7e463def62522aece370d0ebe94863badd824
miro662 <miro662@gmail.com>
2016-02-18 15:54:07 +0100

    Dodano obsługę pauzy

---
+++ Assets/Torch/Scripts/GlobalControllers/PauseController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Komponent odpowiadający za kontrolę pauzy
+/// </summary>
+public class PauseController : MonoBehaviour
+{
+    //Czy gra jest spauzowana?
+    bool paused;
+
+    //Właściwość do zarządzania pauzą
+    public bool Paused
+    {
+        get { return paused; }
+        set
+        {
+            //Zmień pauzę w kontrolerze
+            paused = value;
+
+            //Wywołaj OnPause/OnResume na wszystkich obiektach
+            GameObject[] allGameObjects = Object.FindObjectsOfType<GameObject>();
+            foreach (GameObject oneOfGameObjects in allGameObjects)
+            {
+                if (paused)
+                    oneOfGameObjects.SendMessage("OnPause", SendMessageOptions.DontRequireReceiver);
+                else
+                    oneOfGameObjects.SendMessage("OnResume", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+
+    void Start()
+    {
+        //Gra zaczyna się uruchomiona
+        Paused = true;
+    }
+
+    void Update()
+    {
+        //Sprawdź, czy wciśnięty przycisk pauzy
+        if (Input.GetButtonDown("Pause"))
+        {
+            //Przełącz pauzę
+            Paused = !Paused;
+        }
+    }
+}
---
+++ Assets/Torch/_dev/PauseTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseTester : MonoBehaviour
+{
+	void OnPause()
+    {
+        print("Paused");
+    }
+    void OnResume()
+    {
+        print("Resumed");
+    }
+}
