3f7455d77130d8e5f766099b0d3afac0f4e908ba
miro662 <miro662@gmail.com>
2016-02-18 19:15:37 +0100

    Imported some sprites, created torch controller

---
+++ Assets/Torch/Scripts/LevelMechanics/TorchController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Komponent odpowiadający za zarządzanie tzw. zwykłą świeczką
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class TorchController : Signal
+{
+    #region Animator
+    //Referencja do animatora
+    Animator _animator;
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+
+        //Dodaj funkcje zmieniającą animację do zdarzeń
+        OnSignalOff += UpdateAnimation;
+        OnSignalOn += UpdateAnimation;
+    }
+
+    //Funkcja zmieniająca animację
+    void UpdateAnimation()
+    {
+        _animator.SetBool("Lit", Status);
+    }
+    #endregion
+
+    #region Initialization
+    //Czy świeczka świeci się na początku?
+    public bool isLitOnStart;
+
+    void Start()
+    {
+        Status = isLitOnStart;
+    }
+
+    void OnValidate()
+    {
+        Status = isLitOnStart;
+    }
+    #endregion
+}
