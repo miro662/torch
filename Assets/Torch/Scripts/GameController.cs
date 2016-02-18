34bc7244de3d775323aaca94ad68f2d04d970a30
miro662 <miro662@gmail.com>
2016-02-18 15:34:44 +0100

    Created barebones of GameController class and helper Torch class

---
+++ Assets/Torch/Scripts/GameController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Główny komponent, odpowiada za kontrolę nad grą
+/// </summary>
+public class GameController : MonoBehaviour
+{
+}
---
+++ Assets/Torch/Scripts/Torch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Klasa pomocnicza, zawierająca właściwości ułatwiające dostanie się do różnych ważnych obiektów
+/// </summary>
+public class Torch
+{
+    /// <summary>
+    /// Znajduje obiekt Game Controller i zwraca odpowiednią klasę
+    /// </summary>
+    public GameController GameController
+    {
+        get
+        {
+            GameObject gameControllerGameObject = GameObject.FindGameObjectWithTag("Game Controller");
+            GameController gameController = gameControllerGameObject.GetComponent<GameController>();
+            return gameController;
+        }
+    }
+}
